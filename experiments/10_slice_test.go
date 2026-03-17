6e61935f8754093d0746304563857557540f2ded
TeamMeng <862915265@qq.com>
2026-03-17 16:45:53 +0800

    feature: learn about slice

---
+++ experiments/10_slice_test.go
@@ -0,0 +1,26 @@
+package experiments
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSlice(t *testing.T) {
+	var s []string
+	fmt.Println(s == nil)
+	fmt.Println(len(s) == 0)
+
+	s = make([]string, 3)
+	fmt.Println(s)
+	fmt.Println(s != nil)
+	fmt.Println(len(s))
+	fmt.Println(cap(s))
+
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+	fmt.Println(s)
+
+	s = append(s, "d")
+	fmt.Println(s)
+}
