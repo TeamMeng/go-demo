41c3e540daa330a541f9000bcc642dce30be4b8c
TeamMeng <862915265@qq.com>
2026-05-05 12:54:15 +0800

    feat: add k8s deployments and services

--- webook/main.go
+++ webook/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
-	"github.com/TeamMeng/go-demo/webook/internal/web"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	server := web.InitWeb()
+	// server := web.InitWeb()
+	server := gin.Default()
+
+	server.GET("/hello", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"message": "Hello World"})
+	})
 
 	server.Run(":8080")
 }
